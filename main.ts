bb8ccfad0c8507445e4e43d9a432774e626ebe98
VivaSitani <66580922+VivaSitani@users.noreply.github.com>
2022-08-03 15:17:06 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 let number = 0
 input.onGesture(Gesture.Shake, function () {
     basic.clearScreen()
+    pins.digitalWritePin(DigitalPin.P0, 0)
     number = randint(0, 4)
     if (number == 1) {
         basic.showIcon(IconNames.Yes)
         pins.digitalWritePin(DigitalPin.P0, 1)
     } else if (number == 2) {
         basic.showIcon(IconNames.No)
+        pins.digitalWritePin(DigitalPin.P0, 1)
     } else if (number == 3) {
         basic.showLeds(`
             # # # # #
